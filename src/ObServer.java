bdde15aeef811dac0b1cd5a7f903441f3fbfddcc
namvu26112005 <128945120+vunamdddddds@users.noreply.github.com>
2024-10-19 20:56:42 +0700

    Update ObServer.java

--- src/ObServer.java
+++ src/ObServer.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// lí thuyết : khi sử dụng Obsever bạn phải xác định đối tượng được báo và đối tượng được quan sát 
+
 public class ObServer {
     interface Observer {
         void update(String message);
     }  // tạo 1 interface phương thức update truyền vào tham sô
-   public static class user implements Observer {      // tạo 1 class người dùng
+   public static class user implements Observer {      // tạo 1 class người dùng cái này là đối tượng được báo "có biến động gì thì nó báo luôn cho đối tượng này"
       private   String name;
         public user(String name) {
             this.name = name;
@@ -14,11 +16,11 @@ public class ObServer {
             System.out.println(this.name + ": " + message);
         }
     }
-    static class BookLibrary{   // 1 thư viên chung mọi hoạt động thêm xóa sẽ được thưc thi trong đây
+    static class BookLibrary{   // 1 thư viên chung mọi hoạt động thêm xóa sẽ được thưc thi trong đây  (đây là đối tượng được quan sát) 
         private List<Book> books = new ArrayList<Book>();  // tạo 1 danh  sách chứa sách
-        private List<Observer> observers=new ArrayList<>();  // tạo 1 danh sách chua
+        private List<Observer> observers=new ArrayList<>();  // tạo 1 danh sách chứa danh sách các người dùng trong hệ thống
         public void addObserver(Observer observer) {
-            observers.add(observer);
+            observers.add(observer);  
         }
         public void removeObserver(Observer observer) {
             observers.remove(observer);
